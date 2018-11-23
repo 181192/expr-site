d4f76298a34b7327ebfd1de339b667d7c2214054
181192 <181192@stud.hvl.no>
2018-11-23 23:54:07 +0100

    Add helmet meta data

--- src/html.js
+++ src/html.js
@@ -1,10 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import ogImage from "../static/default-ogimage.png";
+import favicon from "../static/favicon.ico";
 
 export default class HTML extends React.Component {
   render() {
     return (
       <html {...this.props.htmlAttributes} lang="en">
+        <Helmet
+          title={`ExPr - Exam Preparation 2018`}
+          meta={[
+            {
+              name: "description",
+              content: `This page is build to provide all curriculum for the DAT159 subject at HVL. This page is build with Gatsby 💜 (a React based static site generator) and is hosted on Github Pages.`
+            },
+            { property: "og:type", content: "website" },
+            {
+              property: "og:url",
+              content: `https://181192.github.io/expr-site/`
+            },
+            { property: "og:image", content: `${ogImage}` },
+            { property: "og:title", content: `ExPr | Exam Preparation 2018` },
+            {
+              property: "og:description",
+              content: `This page is build to provide all curriculum for the DAT159 subject at HVL. This page is build with Gatsby 💜 (a React based static site generator) and is hosted on Github Pages.`
+            }
+          ]}
+          link={[
+            { rel: "shortcut icon", type: "image/png", href: `${favicon}` }
+          ]}
+        />
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
