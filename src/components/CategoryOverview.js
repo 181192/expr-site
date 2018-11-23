8ee03beac77463b4d34654443ab85b19f1df1e80
181192 <181192@stud.hvl.no>
2018-11-23 20:34:33 +0100

    Add CategoryOverview.js

---
+++ src/components/CategoryOverview.js
@@ -0,0 +1,3 @@
+// This module should show the different categories with a picture
+// When click at we should be re-directed to /news, with the category
+// property such that it displays in list view all the posts in that category
